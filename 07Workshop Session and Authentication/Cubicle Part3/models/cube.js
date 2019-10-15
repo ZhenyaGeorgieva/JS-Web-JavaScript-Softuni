3e146750375a32ec64d950247be9ddb29545a5f6
ZhenyaGeorgieva <zhenya.georgieva@gmail.com>
2019-10-15 11:07:06 +0300

    Adding Workshop Session and Authentication

---
+++ 07Workshop Session and Authentication/Cubicle Part3/models/cube.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const cubeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: String,
+  imageUrl: String,
+  difficultyLevel: Number,
+  accessories: [{ type: mongoose.Types.ObjectId, ref: 'Accessories' }],
+  creatorId: { type: mongoose.Types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Cube', cubeSchema);
+// const mongoose = require('mongoose');
+
+// const cubeSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//     // validate: {
+//     //   validator: function(v){
+//     //     return /\d{3}-\d{3}-\d{3}/.test(v);
+//     //   },
+//     //   message: props=>`${props.value} is not a valid phone number!`
+//     // },
+//   },
+//   description: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: function () {
+//         return this.description.length <= 20;
+//       },
+//       message: 'Description must be less than 20 symbols long!'
+//     }
+//   },
+//   imageUrl: String,
+//   difficultyLevel: Number,
+//   accessories: [{
+//     type: mongoose.Types.ObjectId,
+//     ref: 'Accessories'
+//   }],
+//   creatorId:{
+//     type: mongoose.Types.ObjectId,
+//     ref: 'User'
+//   }
+// });
+
+// module.exports = mongoose.model('Cube', cubeSchema);
+
